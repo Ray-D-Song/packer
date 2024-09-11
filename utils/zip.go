baa6a2198dfbb66a821b6cb8e62381804829c910
Ray-D-Song <songraysmail@gmail.com>
2024-09-11 11:54:55 +0800

    feat: pack sync

--- utils/zip.go
+++ utils/zip.go
@@ -5,42 +5,45 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func Unzip(src string, dest string) error {
-	r, err := zip.OpenReader(src)
+	zipReader, err := zip.OpenReader(src)
 	if err != nil {
 		return err
 	}
-	defer r.Close()
+	defer zipReader.Close()
 
-	for _, f := range r.File {
-		if strings.HasPrefix(f.Name, "dist/") {
-			fpath := filepath.Join(dest, f.Name[len("dist/"):])
-			if f.FileInfo().IsDir() {
-				if err := os.MkdirAll(fpath, os.ModePerm); err != nil {
-					return err
-				}
-				continue
-			}
-			if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+	for _, file := range zipReader.File {
+		filePath := filepath.Join(dest, file.Name)
+
+		if file.FileInfo().IsDir() {
+			if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
 				return err
 			}
-			rc, err := f.Open()
-			if err != nil {
+			continue
+		} else {
+			if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
 				return err
 			}
-			defer rc.Close()
-			w, err := os.Create(fpath)
-			if err != nil {
-				return err
-			}
-			if _, err := io.Copy(w, rc); err != nil {
-				return err
-			}
-			w.Close()
+		}
+
+		srcFile, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+
+		dstFile, err := os.Create(filePath)
+		if err != nil {
+			return err
+		}
+		defer dstFile.Close()
+
+		if _, err := io.Copy(dstFile, srcFile); err != nil {
+			return err
 		}
 	}
+
 	return nil
 }
